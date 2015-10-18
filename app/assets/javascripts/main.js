370e6f36784efbe9e276c444ea1ba3bf83495cbe
Israel Hill <idh@case.edu>
2015-10-18 19:11:20 -0400

    created log in card and transition

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,17 @@
 Parse.initialize("xaWquA962LKda5zgxedPcBxVMwmtwv0tMwWAvLs4", "rEq8Fx7X2melypPqC47pvw4AVNDNJ142tk4Df2j2");
 
-
 var signUpButton = $('#signUp');
+var createAccountButton = $('#create-account-btn');
+
+createAccountButton.click(function() {
+   createAccount();
+});
 
 signUpButton.click(function() {
    signUp();
 });
 
+// add a new user to Parse
 function signUp() {
     var user = new Parse.User();
 
@@ -18,4 +23,13 @@ function signUp() {
     user.set("email", email);
     user.set("password", password);
     user.signUp(null, null);
+}
+
+function createAccount() {
+    $('#sign-in-card').hide();
+    $('#sign-up-card').show();
+    $('#agreement').show();
+    $('#agreement-label').hide();
+    createAccountButton.hide();
+
 }
